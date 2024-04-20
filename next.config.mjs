5914b4676dfd64c286d8a7f3c8a465b9732955c8
medevs <meedfaji@gmail.com>
2024-04-20 19:53:50 +0100

    Add image domain configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["dkeqwsxxlrueydnyrssy.supabase.co"],
+  },
+};
 
 export default nextConfig;
