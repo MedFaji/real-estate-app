4f92ae3e1f26e2b41f863445af562dfda68ccb28
medevs <meedfaji@gmail.com>
2024-04-22 15:48:16 +0100

    Add rent page component with ListingMapView integration

---
+++ app/(routes)/rent/page.jsx
@@ -0,0 +1,12 @@
+import ListingMapView from "@/app/_components/ListingMapView";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="px-10 p-10">
+      <ListingMapView type={"rent"} />
+    </div>
+  );
+};
+
+export default page;
