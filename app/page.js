52ddd075758e91b5fde51ce7f4888c720ffdae49
medevs <meedfaji@gmail.com>
2024-04-22 15:47:08 +0100

    Add ListingMapView component to Home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import ListingMapView from "./_components/ListingMapView";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="px-10 p-10">
+      <ListingMapView type={"sell"} />
+    </div>
+  );
 }
